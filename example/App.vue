c3e2bb44cd79610fbf174b7300b08a4993afb92c
ChenPt <847136903@qq.com>
2018-07-27 17:36:26 +0800

    添加依赖和修改基础文件

--- example/App.vue
+++ example/App.vue
@@ -1,16 +1,78 @@
 <template>
   <div id="app">
-    <public-menu></public-menu>
+    <public-menu :linkSet="linkSet"></public-menu>
   </div>
 </template>
 
 <script>
-import publicMenu from '../src/components/PublicMenu/PublicMenu'
+import publicMenu from '../lib/components/PublicMenu/PublicMenu'
 
 export default {
   name: 'App',
   components: {
     publicMenu
+  },
+  data () {
+    return {
+      linkSet: [
+        {
+          "id": "hmx",
+          "index": 1,
+          "url": "https://hmx-jingwei.huya.com",
+          "name": "作业平台"
+        },
+        {
+          "id": "dmx",
+          "index": 2,
+          "url": "https://dmx-jingwei.huya.com",
+          "test_url": "http://dmx-jingwei.yygamedev.com",
+          "name": "数据源管理系统"
+        },
+        {
+          "id": "onealert",
+          "index": 3,
+          "url": "http://onealert-admin.huya.com/",
+          "test_url": "http://onealert-admin.yygamedev.com",
+          "name": "统一告警平台"
+        },
+        {
+          "id": "deploy",
+          "index": 4,
+          "url": "http://deploytest.jingwei.huya.com/",
+          "name": "统一发布平台"
+        },
+        {
+          "id": "cmdb",
+          "index": 5,
+          "url": "http://cmdb-jingwei.huya.com/",
+          "name": "CMDB管理系统"
+        },
+        {
+          "id": "cdn",
+          "index": 6,
+          "url": "http://cdn.sysop.huya.com",
+          "name": "CDN管理系统"
+        },
+        {
+          "id": "ido",
+          "index": 7,
+          "url": "http://ido.jingwei.huya.com",
+          "name": "IDO"
+        },
+        {
+          "id": "skynet",
+          "index": 8,
+          "url": "https://skynet-jingwei.huya.com/",
+          "name": "Skynet统一监控"
+        },
+        {
+          "id": "autoidc",
+          "index": 9,
+          "url": "http://autoidc-jingwei.huya.com/",
+          "name": "混合云主机交付平台"
+        }
+      ]
+    }
   }
 }
 </script>
--- example/main.js
+++ example/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from '../example/App'
+import iView from 'iview'
+
+Vue.use(iView)
 
 new Vue({
- el: '#app',
- components: {
-   App
- },
- render: h => h(App)
+  el: '#app',
+  components: {
+    App
+  },
+  render: h => h(App)
 })
